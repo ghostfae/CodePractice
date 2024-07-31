2f0eb1d15e5f043c05703be5fa9aa6fe775b089c
kat b <secretlydiamond@gmail.com>
2024-07-31 15:57:00 +0100

    updated with null
    Unsure how to create test for empty; doesn't that already exist?

--- CodeWars/SentenceSmash/Kata.cs
+++ CodeWars/SentenceSmash/Kata.cs
@@ -4,14 +4,17 @@ internal class Kata
 { 
    public static string Smash(string[] words)
    {
-      return String.Join(' ', words);
+      return string.Join(' ', words);
    }
 
    public static string SmashLoop(string[] words)
    {
       string newString = words[0];
+      if (string.IsNullOrEmpty(newString))
+         return string.Empty;
+
       for (int i = 1; i < words.Length; i++)
-      {
+      { 
          newString += ' ' + words[i];
       }
       return newString;
