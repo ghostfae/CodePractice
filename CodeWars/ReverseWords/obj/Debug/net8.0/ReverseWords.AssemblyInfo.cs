07949449c9a1f066cba6dd7e1459c6b55e678277
kat b <secretlydiamond@gmail.com>
2024-10-20 14:46:05 +0100

    added alphabet replacer

--- CodeWars/ReverseWords/obj/Debug/net8.0/ReverseWords.AssemblyInfo.cs
+++ CodeWars/ReverseWords/obj/Debug/net8.0/ReverseWords.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReverseWords")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82bc3565fdb6342cd138d6a17a61f89e0431619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecf6e3bb44207ddd6acbcaf3e4b91b8e47deeb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReverseWords")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReverseWords")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
